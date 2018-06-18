7ee7d4596eb4ea900ed08b6abac7afa13b824bd0
ANITHAS22 <39801902+ANITHAS22@users.noreply.github.com>
2018-06-18 22:20:53 +0530

    Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Sum
+{
+    public static void main(String args[])
+    {
+        Scanner io=new Scanner(System.in);
+        int n=io.nextInt();
+        int n1=io.nextInt();
+        System.out.println((float)Math.pow(n,n1));
+      
+    }
+}
